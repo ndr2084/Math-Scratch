932aa6c55889e787ed6dfa7e632f567135eeb548
Nathan Rous <ndrous2084@gmail.com>
2025-03-19 19:29:46 -0400

    shouldn't take an hour to see an index was set to 0 instead of 1 but fiwb

--- giant_step_baby_step.py
+++ giant_step_baby_step.py
@@ -20,13 +20,15 @@ def baby_step(g: int, m: int, p, flag: int) -> int:
             temp = temp - p
 
     if flag == 0:
+        print("full solution set baby:\n" + str(d))
         return temp
 
 
 def giant_step(h_inner, p_inner, m_inner, m_inverse_remainder):
     key: int = 0
     mir_copy = m_inverse_remainder
-    for i in range(0, m_inner, 1):
+    print(mir_copy)
+    for i in range(1, m_inner, 1):
         answer = m_inverse_remainder * h_inner % p_inner
         d[answer].append(m_inner * i)
         if d[answer].__len__() > 1:
@@ -34,7 +36,7 @@ def giant_step(h_inner, p_inner, m_inner, m_inverse_remainder):
         m_inverse_remainder = m_inverse_remainder * mir_copy
         while m_inverse_remainder > p_inner:
             m_inverse_remainder = m_inverse_remainder - p_inner
-    print("full solution set:\n" + str(d))
+    print("full solution set giant:\n" + str(d))
     return key
 
 def extended_euclidean_algorithm(a: int, b: int):
@@ -42,8 +44,6 @@ def extended_euclidean_algorithm(a: int, b: int):
     if a - b < 0:
         return extended_euclidean_algorithm_helper(b, a, s_1, s_2, t_1, t_2) # b > a
     return extended_euclidean_algorithm_helper(a, b, s_1, s_2, t_1, t_2) # b <= a
-
-    return result
 def extended_euclidean_algorithm_helper(a: int, b: int, s_1: int, s_2: int, t_1: int, t_2: int):
     q: int = 0
     if b == 0:
@@ -73,22 +73,23 @@ def extended_euclidean_algorithm_helper(a: int, b: int, s_1: int, s_2: int, t_1:
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     ##NOTE:
-        #KEY := some integer that's congruent to g^i mod p and some integer that's congruent to h * (g^-1)^(m*j) mod p
-        #VALUE := x, where x = m*j + i
+        #KEY := some integer that's congruent to both g^i mod p and congruent to h * (g^-1)^(m*j) mod p
+        #VALUE := the exponents values i and/or m*j
     save = 1
     discard = 0
-    h: int = 2213
-    p: int = 3571
-    g: int = 650
+    h: int = 21
+    p: int = 71
+    g: int = 11
 
-    bezouts_coefficients = extended_euclidean_algorithm(3571, 650)
-    g_inverse: int = p - bezouts_coefficients[1]  #just use EEA online for now https://www.extendedeuclideanalgorithm.com/calculator.php
+    bezouts_coefficients = extended_euclidean_algorithm(g, p)
+    print(bezouts_coefficients)
+    g_inverse: int = bezouts_coefficients[1]
+    print(g_inverse)
     m: int = giant_step_interval(p)
     d = defaultdict(list)
     baby_step(g, m, p, save)
     inverse_remainder: int = baby_step(g_inverse, m, p, discard)
     key: int = giant_step(h, p, m, inverse_remainder)
-    print(d)
     print(
         "key: " + str(key) + "\n" +
         "value: " + str(d[key]) + "\n" +
